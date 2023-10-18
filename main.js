ceb6d2544da6635bb7ef2b9f0f4a5ea13d3a9f64
analyticscloud1 <148343728+analyticscloud1@users.noreply.github.com>
2023-10-18 23:58:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var ajaxCall = (query, language) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://martin-inviting-wolf.ngrok-free.app",
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        query: query,
+        language: language
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin":"*"
+      },
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
+};
+
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    async post(query, language) {
+      const { response } = await ajaxCall(
+        query,
+        language
+      );
+      //console.log(response.choices[0].text);
+      return response;
+    }
+  }
+  customElements.define("custom-widget", MainWebComponent);
+})();
